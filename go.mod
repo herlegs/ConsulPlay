6e2a9820fb16a426f9e563cbf8b5e323b3be67cf
Yuguang Xiao <xiaoyuguang24@gmail.com>
2020-11-20 11:22:25 +0800

    save

--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
 module github.com/herlegs/ConsulPlay
 
-go 1.12
+replace github.com/hashicorp/vault/api => github.com/hashicorp/vault/api v0.0.0-20200718022110-340cc2fa263f
 
-require github.com/hashicorp/consul/api v1.4.0
+require (
+	github.com/hashicorp/consul v1.8.5
+	github.com/hashicorp/consul/api v1.7.0
+	github.com/hashicorp/vault v1.5.5
+	github.com/hashicorp/vault/api v1.0.5-0.20201104232315-52f641658ed7
+	github.com/hashicorp/vault/sdk v0.1.14-0.20201104232315-52f641658ed7 // indirect
+)
+
+go 1.13
--- vault/main.go
+++ vault/main.go
@@ -1,104 +1,122 @@
 package main
-import (
-"encoding/json"
-"fmt"
-"net/http"
-"time"
 
-"github.com/hashicorp/vault/api"
-"github.com/hashicorp/vault/builtin/credential/aws"
+import (
+	"fmt"
+
+	"github.com/hashicorp/vault/api"
 )
 
 const (
-vaultAddr = "http://YOUR_VAULT_ADDR:8200"
+	vault_addr = "http://127.0.0.1:8200"
 
-staticToken = "YOUR_STATIC_TOKEN"
+	token = "s.MRMenGx0bMAtWZYe7X7BSNEY"
 )
-
-var httpClient = &http.Client{
-Timeout: 10 * time.Second,
-}
 
 func main() {
-// token := staticToken
-//token, err := userpassLogin()
-//if err != nil {
-//	panic(err)
+	config := &api.Config{
+		Address: vault_addr,
+	}
+	client, err := api.NewClient(config)
+	if err != nil {
+		fmt.Printf("err init client:%v\n", err)
+		return
+	}
+	client.SetToken(token)
+	secret, err := client.Logical().Read("secret/data/hello")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("data:%#v\n", secret)
+	for k, v := range secret.Data {
+		fmt.Printf("key: %v, value: %v\n", k, v)
+	}
+}
+
+//var httpClient = &http.Client{
+//	Timeout: 10 * time.Second,
 //}
-token, err := awsLogin()
-if err != nil {
-panic(err)
-}
-
-client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
-if err != nil {
-panic(err)
-}
-
-client.SetToken(token)
-data, err := client.Logical().Read("secret/data/my-secret")
-if err != nil {
-panic(err)
-}
-
-b, _ := json.Marshal(data.Data)
-fmt.Println(string(b))
-}
-
-const (
-username = "jun06t"
-password = "foo"
-)
-
-func userpassLogin() (string, error) {
-// create a vault client
-client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
-if err != nil {
-return "", err
-}
-
-// to pass the password
-options := map[string]interface{}{
-"password": password,
-}
-path := fmt.Sprintf("auth/userpass/login/%s", username)
-
-// PUT call to get a token
-secret, err := client.Logical().Write(path, options)
-if err != nil {
-return "", err
-}
-
-token := secret.Auth.ClientToken
-return token, nil
-}
-
-const (
-accessKey    = ""
-secretKey    = ""
-sessionToken = ""
-headerValue  = ""
-)
-
-func awsLogin() (string, error) {
-// get aws credential
-data, err := awsauth.GenerateLoginData(accessKey, secretKey, sessionToken, headerValue)
-if err != nil {
-return "", err
-}
-
-// create a vault client
-client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
-if err != nil {
-return "", err
-}
-
-// PUT call to get a token
-secret, err := client.Logical().Write("auth/aws/login", data)
-if err != nil {
-return "", err
-}
-
-token := secret.Auth.ClientToken
-return token, nil
-}
+//
+//func main() {
+//	// token := staticToken
+//	//token, err := userpassLogin()
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+//	token, err := awsLogin()
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	client.SetToken(token)
+//	data, err := client.Logical().Read("secret/hello")
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	b, _ := json.Marshal(data.Data)
+//	fmt.Println(string(b))
+//}
+//
+//const (
+//	username = "jun06t"
+//	password = "foo"
+//)
+//
+//func userpassLogin() (string, error) {
+//	// create a vault client
+//	client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	// to pass the password
+//	options := map[string]interface{}{
+//		"password": password,
+//	}
+//	path := fmt.Sprintf("auth/userpass/login/%s", username)
+//
+//	// PUT call to get a token
+//	secret, err := client.Logical().Write(path, options)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	token := secret.Auth.ClientToken
+//	return token, nil
+//}
+//
+//const (
+//	accessKey    = ""
+//	secretKey    = ""
+//	sessionToken = ""
+//	headerValue  = ""
+//)
+//
+//func awsLogin() (string, error) {
+//	// get aws credential
+//	data, err := awsauth.GenerateLoginData(accessKey, secretKey, sessionToken, headerValue)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	// create a vault client
+//	client, err := api.NewClient(&api.Config{Address: vaultAddr, HttpClient: httpClient})
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	// PUT call to get a token
+//	secret, err := client.Logical().Write("auth/aws/login", data)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	token := secret.Auth.ClientToken
+//	return token, nil
+//}
